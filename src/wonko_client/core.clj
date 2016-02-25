f84ba5340761b15affcc5f799a6d54ab7e541d0e
Srihari Sriraman <srihari@nilenso.com>
2016-02-25 18:49:36 +0530

    Send basic event metadata with every event.
    - IP address, hostname, and a timestamp
    - This is sent under :metadata key for clear separation from other
      metric information.

--- src/wonko_client/core.clj
+++ src/wonko_client/core.clj
@@ -1,11 +1,18 @@
 (ns wonko-client.core
-  (:require [wonko-client.kafka-producer :as kp]))
+  (:require [wonko-client.kafka-producer :as kp]
+            [wonko-client.util :as util]))
 
 (defonce ^String service
   (atom ""))
 
+(defn metadata []
+  {:host (util/hostname)
+   :ip-address (util/ip-address)
+   :ts (util/current-timestamp)})
+
 (defn counter [metric-name properties & {:as options}]
   (kp/send-message {:service @service
+                    :metadata (metadata)
                     :metric-type :counter
                     :metric-name metric-name
                     :properties properties
@@ -14,6 +21,7 @@
 
 (defn gauge [metric-name properties metric-value & {:as options}]
   (kp/send-message {:service @service
+                    :metadata (metadata)
                     :metric-type :gauge
                     :metric-name metric-name
                     :properties properties
@@ -23,11 +31,12 @@
 
 (defn alert [alert-name alert-info]
   (kp/send-message {:service @service
+                    :metadata (metadata)
                     :alert-name alert-name
                     :alert-info alert-info
-                    :metric-type :counter
                     :metric-name alert-name
-                    :properties {}}
+                    :properties {}
+                    :metric-type :counter}
                    "wonko-alerts"))
 
 (defn init! [service-name kafka-config]
--- src/wonko_client/util.clj
+++ src/wonko_client/util.clj
@@ -10,3 +10,16 @@
 
 (defn stop-daemon [daemon]
   (.interrupt ^Thread daemon))
+
+(defn hostname
+  "Returns the current host name"
+  []
+  (.getHostName (java.net.InetAddress/getLocalHost)))
+
+(defn ip-address
+  "Returns the string representation of the IP of the local host"
+  []
+  (.getHostAddress (java.net.InetAddress/getLocalHost)))
+
+(defn current-timestamp []
+  (System/currentTimeMillis))

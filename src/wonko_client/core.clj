60ad8e06eb2deb8d3bbc771a8a43a1b5f459ba31
Srihari Sriraman <srihari@nilenso.com>
2016-02-29 09:48:04 +0530

    Support streams
    - Add 'stream' endpoint
    - Refactor building of the message to extract common parts

--- src/wonko_client/core.clj
+++ src/wonko_client/core.clj
@@ -14,33 +14,31 @@
    :ip-address (util/ip-address)
    :ts (util/current-timestamp)})
 
+(defn message [metric-name properties metric-value options metric-type]
+  {:service      @service
+   :metadata     (metadata)
+   :metric-name  metric-name
+   :metric-type  metric-type
+   :metric-value metric-value
+   :properties   properties
+   :options      options})
+
 (defn counter [metric-name properties & {:as options}]
-  (kp/send-message {:service @service
-                    :metadata (metadata)
-                    :metric-type :counter
-                    :metric-name metric-name
-                    :properties properties
-                    :options options}
+  (kp/send-message (message metric-name properties nil options :counter)
                    (:events @topics)))
 
 (defn gauge [metric-name properties metric-value & {:as options}]
-  (kp/send-message {:service @service
-                    :metadata (metadata)
-                    :metric-type :gauge
-                    :metric-name metric-name
-                    :properties properties
-                    :metric-value metric-value
-                    :options options}
+  (kp/send-message (message metric-name properties metric-value options :gauge)
+                   (:events @topics)))
+
+(defn stream [metric-name properties metric-value & {:as options}]
+  (kp/send-message (message metric-name properties metric-value options :stream)
                    (:events @topics)))
 
 (defn alert [alert-name alert-info]
-  (kp/send-message {:service @service
-                    :metadata (metadata)
-                    :alert-name alert-name
-                    :alert-info alert-info
-                    :metric-name alert-name
-                    :properties {}
-                    :metric-type :counter}
+  (kp/send-message (merge (message alert-name {} nil nil :counter)
+                          {:alert-name alert-name
+                           :alert-info alert-info})
                    (:alerts @topics)))
 
 (defn set-topics! [events-topic alerts-topic]

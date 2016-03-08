e878da7c26bed9c3900fb1027716294d3f204dc4
Timothy Andrew <mail@timothyandrew.net>
2016-03-08 10:04:09 +0530

    Two metrics with the same name but different type are different metrics.

--- src/wonko_client/validation.clj
+++ src/wonko_client/validation.clj
@@ -2,15 +2,22 @@
 
 (defonce metric->label-names (atom {}))
 
-(defn- maybe-set-label-names-for-metric [metric->label-names service metric-name label-names]
-  (if (metric->label-names [service metric-name])
-    metric->label-names
-    (assoc metric->label-names [service metric-name] label-names)))
+(defn- metric-key
+  "A unique representation of a given metric, used as a key
+   in `metric->label-names'"
+  [{:keys [service metric-name metric-type] :as message}]
+  [service metric-name metric-type])
 
-(defn valid? [{:keys [service metric-name properties] :as message}]
+(defn- maybe-set-label-names-for-metric
+  [metric->label-names message label-names]
+  (if (metric->label-names (metric-key message))
+    metric->label-names
+    (assoc metric->label-names (metric-key message) label-names)))
+
+(defn valid? [{:keys [service metric-name properties metric-type] :as message}]
   (let [label-names (set (keys properties))]
-    (swap! metric->label-names maybe-set-label-names-for-metric service metric-name label-names)
-    (= label-names (@metric->label-names [service metric-name]))))
+    (swap! metric->label-names maybe-set-label-names-for-metric message label-names)
+    (= label-names (@metric->label-names (metric-key message)))))
 
 (defn validate! [message]
   (when-not (valid? message)
--- test/wonko_client/validation_test.clj
+++ test/wonko_client/validation_test.clj
@@ -7,13 +7,17 @@
 
 (deftest validate-label-names-test
   (testing "valid when label names have not changed"
-    (is (v/valid? {:service "label-names" :metric-name "not-changed" :properties {"first" 1 "second" 2}}))
-    (is (v/valid? {:service "label-names" :metric-name "not-changed" :properties {"first" 3 "second" 4}})))
+    (is (v/valid? {:service "label-names" :metric-name "not-changed" :metric-type :counter :properties {"first" 1 "second" 2}}))
+    (is (v/valid? {:service "label-names" :metric-name "not-changed" :metric-type :counter :properties {"first" 3 "second" 4}})))
 
   (testing "not valid when label names have been added"
-    (is (v/valid? {:service "label-names" :metric-name "added" :properties {"first" 1}}))
-    (is (not (v/valid? {:service "label-names" :metric-name "added" :properties {"first" 3 "second" 4}}))))
+    (is (v/valid? {:service "label-names" :metric-name "added" :metric-type :counter :properties {"first" 1}}))
+    (is (not (v/valid? {:service "label-names" :metric-name "added" :metric-type :counter :properties {"first" 3 "second" 4}}))))
 
   (testing "not valid when label names have been removed"
-    (is (v/valid? {:service "label-names" :metric-name "removed" :properties {"first" 3 "second" 4}}))
-    (is (not (v/valid? {:service "label-names" :metric-name "removed" :properties {"first" 1}})))))
+    (is (v/valid? {:service "label-names" :metric-name "removed" :metric-type :counter :properties {"first" 3 "second" 4}}))
+    (is (not (v/valid? {:service "label-names" :metric-name "removed" :metric-type :counter :properties {"first" 1}}))))
+
+  (testing "metrics with the same name but different type are considered different metrics"
+    (is (v/valid? {:service "label-names" :metric-name "removed" :metric-type :counter :properties {"first" 3 "second" 4}}))
+    (is (v/valid? {:service "label-names" :metric-name "removed" :metric-type :alert :properties {"first" 1}}))))

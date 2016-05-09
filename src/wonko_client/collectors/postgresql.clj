d5b20d0d9a33b0cc2548fe501604576c511a5c55
Srihari Sriraman <srihari@nilenso.com>
2016-05-09 09:56:18 +0530

    Convert pg stats to wonko metrics
    - Describe which columns go as properties, and consequently which don't.
    - Use compiler's munging function to clean SQL statements before sending.

--- src/wonko_client/collectors/postgresql.clj
+++ src/wonko_client/collectors/postgresql.clj
@@ -1,6 +1,7 @@
 (ns wonko-client.collectors.postgresql
   (:require [clojure.java.jdbc :as j]
-            [clojure.java.io :as io])
+            [clojure.java.io :as io]
+            [clojure.string :as s])
   (:import [java.sql SQLException]))
 
 (defonce conn
@@ -14,13 +15,51 @@
   (j/execute! @conn (read-prepared-stmts))
   nil)
 
-(defn get-data []
+(defn stats []
   (let [stmts (j/query @conn "select * from pg_prepared_statements where from_sql is true")]
-    (into {}
-          (for [stmt stmts
-                :let [q-name (:name stmt)
-                      q (str "EXECUTE " q-name)]]
-            {q-name (j/query @conn q)}))))
+    (->> (for [stmt stmts
+               :let [q-name (:name stmt)
+                     q (str "EXECUTE " q-name)]]
+           {q-name (vec (j/query @conn q))})
+         (into {}))))
+
+
+(defn ->wonko-name [query-str]
+  (if (some? query-str)
+    (-> query-str
+        (clojure.lang.Compiler/munge)
+        (s/replace #"[^\p{L}\p{Nd}]+" "_"))
+    ""))
+
+(def category-properties
+  {"queries" #{:query}
+   "index_usage" #{:relation :indexname}
+   "relation_sizes" #{:relation}
+   "tuple_info" #{:relation}
+   "table_and_index_bloat" #{:relation :iname}
+   "db_size" #{}
+   "cache_total" #{}
+   "table_sizes" #{:relation}
+   "cache_tables" #{:relation}
+   "table_bloat" #{:relation :is_na}})
+
+(defn row->wonko-metrics [category row]
+  (let [property-names (get category-properties category)
+        properties (->> property-names
+                        (map row)
+                        (map ->wonko-name)
+                        (zipmap property-names))
+        stats (apply dissoc row property-names)]
+    (for [[stat-name stat-value] stats
+          :when (some? stat-value)]
+      {:metric-name (str "pg_" category)
+       :properties (assoc properties :stat-name (name stat-name))
+       :metric-value stat-value})))
 
 (defn send-metrics []
-  nil)
+  (doall
+   (for [[category rows] (stats)
+         row rows
+         :let [{:keys [metric-name metric-value properties]}
+               (row->wonko-metrics category row)]]
+     (client/gauge metric-name properties metric-value))))

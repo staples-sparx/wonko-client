2ed18525b1f8d18611a0ec648e6e1107139ac7a3
Srihari Sriraman <srihari@nilenso.com>
2016-05-10 14:33:17 +0530

    Use a static var instead of atom to hold `instance`.
    - `instance` is never written/modified to except during init. Rest of the
      operations are reads, which do not require the additional locking
      mechanism that atoms provide. This expresses the relatively immutable
      nature of the instance more clearly than an atom.

--- src/wonko_client/core.clj
+++ src/wonko_client/core.clj
@@ -14,10 +14,10 @@
                       :alerts "wonko-alerts"}})
 
 (defonce instance
-  (atom {:service nil
-         :topics nil
-         :thread-pool nil
-         :producer nil}))
+  {:service nil
+   :topics nil
+   :thread-pool nil
+   :producer nil})
 
 (defn- send-sync [{:keys [thread-pool topics producer] :as instance} topic message]
   (kp/send producer message (get topics topic)))
@@ -27,23 +27,23 @@
 
 (defn counter [metric-name properties & {:as options}]
   (->> :counter
-       (message/build (:service @instance) metric-name properties nil options)
-       (send-async @instance :events)))
+       (message/build (:service instance) metric-name properties nil options)
+       (send-async instance :events)))
 
 (defn gauge [metric-name properties metric-value & {:as options}]
   (->> :gauge
-       (message/build (:service @instance) metric-name properties metric-value options)
-       (send-async @instance :events)))
+       (message/build (:service instance) metric-name properties metric-value options)
+       (send-async instance :events)))
 
 (defn stream [metric-name properties metric-value & {:as options}]
   (->> :stream
-       (message/build (:service @instance) metric-name properties metric-value options)
-       (send-async @instance :events)))
+       (message/build (:service instance) metric-name properties metric-value options)
+       (send-async instance :events)))
 
 (defn alert [alert-name alert-info]
   (->> :counter
-       (message/build-alert (:service @instance) alert-name {} nil alert-name alert-info nil)
-       (send-sync @instance :alerts)))
+       (message/build-alert (:service instance) alert-name {} nil alert-name alert-info nil)
+       (send-sync instance :alerts)))
 
 (defn set-topics! [events-topic alerts-topic]
   (swap! instance assoc :topics {:events events-topic
@@ -52,15 +52,16 @@
 (defn init! [service-name kafka-config & {:as user-options}]
   (let [options (merge default-options user-options)
         {:keys [validate? thread-pool-size queue-size topics]} options]
-    (reset! instance
-            {:service service-name
-             :topics topics
-             :thread-pool (util/create-fixed-threadpool options)
-             :producer (kp/create kafka-config options)})
+    (alter-var-root #'instance
+                    (constantly
+                     {:service service-name
+                      :topics topics
+                      :thread-pool (util/create-fixed-threadpool options)
+                      :producer (kp/create kafka-config options)}))
     (v/set-validation! validate?)
     (log/info "wonko-client initialized" instance)
     nil))
 
 (defn terminate! []
-  (kp/close @instance)
-  (util/stop-tp (:thread-pool @instance)))
+  (kp/close instance)
+  (util/stop-tp (:thread-pool instance)))

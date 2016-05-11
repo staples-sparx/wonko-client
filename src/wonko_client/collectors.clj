406ceecc2201d39b493e3752ffac23c4713cc4ef
Srihari Sriraman <srihari@nilenso.com>
2016-05-11 19:03:57 +0530

    Make starting and stopping collectors idempotent.
    - Do not throw errors when trying to stop a stopped collector or
      starting a started collector.

--- src/wonko_client/collectors.clj
+++ src/wonko_client/collectors.clj
@@ -28,21 +28,22 @@
   (some? (get @collectors collector-name)))
 
 (defn start [collector-name collect-fn options]
-  (when (running? collector-name)
-    (throw (ex-info "Collector is already running" {:collector-name collector-name})))
-  (let [{:keys [rate-ms] :as config} (merge (get default-config collector-name) options)
-        tp (util/create-scheduled-tp (->collector-fn collector-name collect-fn) rate-ms)]
-    (log/info "Started collector" collector-name "with config" config)
-    (swap! collectors assoc collector-name tp)
-    nil))
+  (if (running? collector-name)
+    (log/warn "The collector is already running" {:collector-name collector-name})
+    (let [{:keys [rate-ms] :as config} (merge (get default-config collector-name) options)
+          tp (util/create-scheduled-tp (->collector-fn collector-name collect-fn) rate-ms)]
+      (log/info "Started collector" collector-name "with config" config)
+      (swap! collectors assoc collector-name tp)
+      nil)))
 
 (defn stop [collector-name]
-  (when (not (running? collector-name))
-    (throw (ex-info "The collector isn't running" {:collector-name collector-name})))
-  (util/stop-tp (get @collectors collector-name))
-  (log/info "Stopped collector" collector-name)
-  (swap! collectors dissoc collector-name)
-  nil)
+  (if (not (running? collector-name))
+    (log/warn "The collector isn't running" {:collector-name collector-name})
+    (do
+      (util/stop-tp (get @collectors collector-name))
+      (log/info "Stopped collector" collector-name)
+      (swap! collectors dissoc collector-name)
+      nil)))
 
 (defn start-host-metrics [& {:as options}]
   (start :host-metrics hm/send-metrics options))
@@ -56,5 +57,5 @@
     (start :postgresql #(collect-fn get-conn-fn) options)))
 
 (defn stop-all []
-  (doseq [collector-name (filter running? (keys @collectors))]
+  (doseq [collector-name (keys @collectors)]
     (stop collector-name)))

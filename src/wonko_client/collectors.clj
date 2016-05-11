ac121c36c9bdbf10c6cbc1f2bc0fa9b837a9fdd0
Srihari Sriraman <srihari@nilenso.com>
2016-05-11 14:59:00 +0530

    Allow passing in the get-conn-fn to the postgresql collector
    - This changes the collectors interface (again) to give clarity to
      inputs needed to start the postgresql collector.
    - This interface also allows for passing optional arguments to
      collectors.
    - Extract the default configurations from the collectors atom. The
      configurations and the thread pools are different concerns, and
      keeping them together complects things.
    - Add an additional arity in the start fn to accomodate init-fn. The
      init-fn is run exactly once after validation, and before starting the
      thread.

--- src/wonko_client/collectors.clj
+++ src/wonko_client/collectors.clj
@@ -4,11 +4,17 @@
             [wonko-client.util :as util]
             [clojure.tools.logging :as log]))
 
+(def default-config
+  {:host-metrics {:rate-ms 5000}
+   :ping         {:rate-ms 5000}
+   :postgresql   {:rate-ms 60000}})
+
 (defonce collectors
+  ^{:doc "Map name of collector to the thread(pool) that it runs in."}
   (atom
-   {:host-metrics {:f hm/send-metrics :rate-ms 5000 :tp nil}
-    :ping         {:f ping/send-ping :rate-ms 5000 :tp nil}
-    :postgresql   {:f nil :rate-ms 60000 :tp nil}}))
+   {:host-metrics nil
+    :ping nil
+    :postgresql nil}))
 
 (defn ->collector-fn [collector-name f]
   (fn []
@@ -19,31 +25,40 @@
         (log/error e "Unable to collect metrics" collector-name)))))
 
 (defn running? [collector-name]
-  (some? (get-in @collectors [collector-name :tp])))
+  (some? (get @collectors collector-name)))
 
-(defn conditionally-include-collector [collector-name]
-  (when (= collector-name :postgresql)
-    (require 'wonko-client.collectors.postgresql)
-    (let [f (ns-resolve (find-ns 'wonko-client.collectors.postgresql) 'send-metrics)]
-      (swap! collectors assoc-in [:postgresql :f] f))))
-
-(defn start [collector-name & {:as options}]
-  (when (running? collector-name)
-    (throw (ex-info "Collector is already running" {:collector-name collector-name})))
-  (log/info "Starting collector" collector-name)
-  (conditionally-include-collector collector-name)
-  (let [{:keys [f rate-ms]} (merge (get @collectors collector-name) options)
-        tp (util/create-scheduled-tp (->collector-fn collector-name f) rate-ms)]
-    (swap! collectors assoc collector-name {:f f :rate-ms rate-ms :tp tp})
-    nil))
+(defn start
+  ([collector-name collect-fn options]
+     (start collector-name (constantly nil) collect-fn options))
+  ([collector-name init-fn collect-fn options]
+     (when (running? collector-name)
+       (throw (ex-info "Collector is already running" {:collector-name collector-name})))
+     (init-fn)
+     (let [{:keys [rate-ms] :as config} (merge (get default-config collector-name) options)
+           tp (util/create-scheduled-tp (->collector-fn collector-name collect-fn) rate-ms)]
+       (log/info "Started collector" collector-name "with config" config)
+       (swap! collectors assoc collector-name tp)
+       nil)))
 
 (defn stop [collector-name]
   (when (not (running? collector-name))
     (throw (ex-info "The collector isn't running" {:collector-name collector-name})))
-  (log/info "Stopping collector" collector-name)
-  (util/stop-tp (get-in @collectors [collector-name :tp]))
-  (swap! collectors update collector-name dissoc :tp)
+  (util/stop-tp (get @collectors collector-name))
+  (log/info "Stopped collector" collector-name)
+  (swap! collectors dissoc collector-name)
   nil)
+
+(defn start-host-metrics [& {:as options}]
+  (start :host-metrics hm/send-metrics options))
+
+(defn start-ping [& {:as options}]
+  (start :ping ping/send-ping options))
+
+(defn start-postgresql [get-conn-fn & {:as options}]
+  (require 'wonko-client.collectors.postgresql)
+  (let [init-fn (ns-resolve (find-ns 'wonko-client.collectors.postgresql) 'init)
+        collect-fn (ns-resolve (find-ns 'wonko-client.collectors.postgresql) 'send-metrics)]
+    (start :postgresql #(init-fn get-conn-fn) collect-fn options)))
 
 (defn stop-all []
   (doseq [collector-name (filter running? (keys @collectors))]

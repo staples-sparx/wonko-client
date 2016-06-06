1dfb3258b12ae7955e091bee16953da34425c652
Srihari Sriraman <srihari@nilenso.com>
2016-06-06 10:13:19 +0530

    Use a more robust strategy to ensure pg_stat_statements is loaded correctly.
    - presence in pg_available_extenstions is not sufficient to guarrantee
      that the pg_stat_statements extension is loaded correctly.

--- resources/monitor-postgresql.sql
+++ resources/monitor-postgresql.sql
@@ -200,6 +200,6 @@ SELECT LEFT(query,50) AS query,
        calls, total_time, rows, shared_blks_hit,
        local_blks_hit, blk_read_time, blk_write_time
 FROM pg_stat_statements
-WHERE EXISTS(SELECT * FROM pg_available_extensions
-             WHERE name = 'pg_stat_statements')
+WHERE (SELECT current_setting('shared_preload_libraries')
+       LIKE '%pg_stat_statements%')
 ORDER BY calls DESC;
